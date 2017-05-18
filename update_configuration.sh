ad69c2198bbf454188d156693e40b2ce9acd9abc
Andrew Kirkpatrick <andrew.kirkpatrick@unata.com>
2017-05-18 10:03:37 -0400

    Testing Git commit bits

--- update_configuration.sh
+++ update_configuration.sh
@@ -3,9 +3,17 @@
 RELEASE=$1
 YAML_KEY="backend_version"
 CONFIGURATION_FILE="configuration.yml"
+COMMIT_MESSAGE="Released ${RELEASE} of Project to Client by User"
 
 # Replace configuration value in yaml file
 sed -i '' "s/\($YAML_KEY *: *\).*/\1$RELEASE/" $CONFIGURATION_FILE
 
-COMMIT_MESSAGE="Released ${RELEASE} of Project to Client by User"
-echo $COMMIT_MESSAGE
+# Commit the updated configuration yaml file
+GIT_ADD="git add ${CONFIGURATION_FILE}"
+GIT_COMMIT="git commit -m \"${COMMIT_MESSAGE}\""
+GIT_PUSH="git push"
+
+eval $GIT_ADD
+eval $GIT_COMMIT
+echo $GIT_ADD
+echo $GIT_COMMIT
